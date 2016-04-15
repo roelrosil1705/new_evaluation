8e9cd5deb1839d5c8792523f3c0bda4b2b5691c4
Henryblinds <hbg_009@ymail.com>
2016-04-15 11:15:03 +0800

    Modified question

--- app/src/main/java/com/cloudwalkdigital/activation/evaluationapp/QuestionActivity.java
+++ app/src/main/java/com/cloudwalkdigital/activation/evaluationapp/QuestionActivity.java
@@ -1,7 +1,11 @@
 package com.cloudwalkdigital.activation.evaluationapp;
 
+import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class QuestionActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class QuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+        DataBaseHelper myDbHelper = new DataBaseHelper(this);
+        try {
+            myDbHelper.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+        try {
+            myDbHelper.openDataBase();
+        }catch(SQLException sqle){
+            throw sqle;
+        }
+
+        String[][] text = myDbHelper.getDepartments();
+        int cc = text.length - 1;
+        for(int i=0;i<=cc;i++){
+            Toast.makeText(getApplicationContext(),text[i][1]+"",Toast.LENGTH_LONG).show();
+        }
+
     }
 }
